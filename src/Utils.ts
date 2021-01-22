3ac3db47a8abb5db2168fd0855a4a1dd8e03d3ab
ethuringer <ethuringer@gmail.com>
2021-01-22 12:56:04 +0100

    parameter has been changed

--- src/Utils.ts
+++ src/Utils.ts
@@ -46,7 +46,7 @@ class Utils extends Debouncer {
      * @param el {Element} - egy DOM Element típus
      * @returns {void}
      */
-    static removeNode(el: HTMLElement): void {
+    static removeNode(el: Element): void {
         el.parentNode.removeChild(el);
     }
 
